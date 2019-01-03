a11005f85947463d4aae8e0b61cad1dd6f5c314f
Sergey <xxsmotur@gmail.com>
2019-01-03 18:31:43 +0500

    Add method for make provider

--- src/jams/app.py
+++ src/jams/app.py
@@ -4,6 +4,7 @@ class App(object):
     def __init__(self, description, version, command_manager):
         self.command_manager = command_manager
         self._provider = None
+        self._providers_inn = dict(github=GithubProvider)
     
     def build(self, url):
         pass
@@ -14,3 +15,9 @@ class App(object):
             self._provider = GithubProvider()
         if url.find('gitlab.com'):
             self._provider = GitlabProvider()
+
+    def make_provider(self, provider_name):
+        '''
+            initialization of provider
+        '''
+        self._provider = self._providers_inn[provider_name]()
