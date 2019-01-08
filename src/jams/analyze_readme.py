a20196cae8c95212afd3765be43d77a90dc7c262
Sergey <xxsmotur@gmail.com>
2019-01-08 14:17:16 +0500

    using of separate method for check_ci

--- src/jams/analyze_readme.py
+++ src/jams/analyze_readme.py
@@ -12,6 +12,13 @@ class AnalyzeReadme(Analyze):
         check_ci provides checking of README.md
         containce badge with ci provider
         '''
+        return self._check_ci(repo)
+    
+    def _check_ci(self, repo):
+        '''
+        this method trying to parse README.md and find
+        badge which is point to CI provider
+        ''' 
         if len(list(filter(lambda x: self._content.find('{0}/{1}'.format(x, repo)) != -1, self._ci))) > 0:
             return 0
         return 1
