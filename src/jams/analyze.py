b0901f4d96745683a39171c7a4fbc3a720219309
Sergey <xxsmotur@gmail.com>
2019-01-08 14:10:12 +0500

    Add comments to Analyze method

--- src/jams/analyze.py
+++ src/jams/analyze.py
@@ -1,4 +1,18 @@
 
 class Analyze:
+    """
+    This class defines base class for analyze content
+    """
+
     def __init__(self, content):
         self._content = content
+    
+    def __str__(self):
+        return self._content
+    
+    def check(self, repo):
+        '''
+        check is a main method for making analyzation
+        of content
+        '''
+        raise NotImplementedError
