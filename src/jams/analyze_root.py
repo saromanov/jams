077d3ebcfd2ec850a5c15be738a7d55c11caafc7
Sergey <xxsmotur@gmail.com>
2019-01-28 18:20:57 +0500

    Add method for get licence from repo

--- src/jams/analyze_root.py
+++ src/jams/analyze_root.py
@@ -5,10 +5,20 @@ class AnalyzeRoot(Checker):
     """provides checker for analyze root structure 
     of the project. It can be check licence, contributing, make file, etc
     """
-    def __init__(self):
+    def __init__(self, provider):
         super().__init__()
+        self._provider = provider
     
     def start_message(self):
         """return message before start of checkers
         """
         return 'Checking of the root structure'
+
+    def _get_licence(self):
+        '''trying to get licence from repo
+        '''
+        try:
+             return self._provider.get_content_file(
+                    self._url, 'LICENCE')
+        except Exception:
+            return None
