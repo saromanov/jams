3fa86fef816358296bf5890a858cf5f44be7baf9
Sergey <xxsmotur@gmail.com>
2019-01-28 17:23:16 +0500

    Change name of the method

--- src/jams/github_provider.py
+++ src/jams/github_provider.py
@@ -8,7 +8,7 @@ class GithubProvider(Provider):
         Provider.__init__(self, token)
         self.client = Github(token)
 
-    def get_repository(self, name):
+    def get_repo(self, name):
         '''
             return repository by the name
         '''
@@ -17,12 +17,12 @@ class GithubProvider(Provider):
     def get_open_issues(self, name):
         '''returns list of open issues
         '''
-        repo = self.client.get_repo(name)
+        repo = self.get_repo(name)
         return repo.get_issues(state='open')
 
     def get_content_file(self, repo, name):
         '''returns specific content file
            before return, file will be decoded via base64
         '''
-        r = self.get_repository(repo)
+        r = self.get_repo(repo)
         return base64.b64decode(r.get_contents(name).content)
