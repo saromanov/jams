a70ca2ca6988c52d18fe9e078cdfd271f7840dd1
Sergey <xxsmotur@gmail.com>
2019-01-31 18:23:30 +0500

    Score: add_check method

--- src/jams/score.py
+++ src/jams/score.py
@@ -8,8 +8,14 @@
 class Score:
     def __init__(self):
         self._score = 0
+        self._checks = {}
     
     def add_total_checks(self, num):
         ''' setting total number of checks
         '''
         self._total = num
+    
+    def add_check(self, name, score):
+        ''' setting check to the known checks
+        '''
+        self._checks[name] = score
