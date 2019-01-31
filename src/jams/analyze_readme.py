f112cadfd911360f1858e08d2b6db913b1f10032
Sergey <xxsmotur@gmail.com>
2019-01-31 18:24:53 +0500

    using of add check methods

--- src/jams/analyze_readme.py
+++ src/jams/analyze_readme.py
@@ -35,6 +35,7 @@ class AnalyzeReadme(Checker):
         result = len(list(filter(lambda x: self._content.find(
             '{0}/{1}'.format(x, repo)) != -1, self._ci))) > 0
         output('Checking Badge for CI', result)
+        self.score.add_check('Checking Badge for CI', result)
         return 0 if result else 1
 
     def _check_quality_report(self):
@@ -44,6 +45,7 @@ class AnalyzeReadme(Checker):
         '''
         result = self._content.find('goreportcard.com') > 0
         output('Checking Badge for code quality', result)
+        self.score.add_check('Checking Badge for code quality', result)
         return 1 if result else 0
 
     def _check_title(self):
@@ -53,10 +55,12 @@ class AnalyzeReadme(Checker):
         '''
         result = self._content.startswith('# ')
         output('Checking correct title', result)
+        self.score.add_check('Checking correct title', result)
         return 0 if result else 1
 
     def _check_overview(self):
         result = self._content.find(
             '## Getting Started') > 0 or self._content.find('## Overview') > 0
         output('Not contains Getting Started or Overflow parts', result)
+        self.score.add_check('Not contains Getting Started or Overflow parts', result)
         return 0 if result else 1
