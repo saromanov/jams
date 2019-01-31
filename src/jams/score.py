65bc65f37039175732968f4d19d6c9eb7bfe4ae8
Sergey <xxsmotur@gmail.com>
2019-01-31 18:18:14 +0500

    init for Score method

---
+++ src/jams/score.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    jams.score
+    ~~~~~~~~~~~~~
+    Implementing score method for calculation of score from checkers 
+"""
+
+class Score:
+    def __init__(self):
+        self._score = score
