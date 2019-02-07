0971307ca2e1f7f37cf8ed0a976ad4b5e53d4689
Sergey <xxsmotur@gmail.com>
2019-02-07 18:38:56 +0500

    check docker: add checking of latest tag

--- src/jams/analyze_dockerfile.py
+++ src/jams/analyze_dockerfile.py
@@ -4,8 +4,19 @@ from score import Score
 
 class AnalyzeDockerfile(Checker):
     """ provides analization of the Dockerfile
+    https://gist.github.com/Faheetah/a2a401a01d2d56fa7d1a9d7ab0d2831b
     """
     def __init__(self, content):
         self._content = str(content)
         self.score = Score()
         super().__init__(str(content))
+    
+    def _check_latest(self):
+        '''check if Docker file contains pulling from :latest tag
+        '''
+        docker_file = self._get_dockerfile()
+        if not docker_file:
+            return 1
+        if len(docker_file.find(':latest')) > 0:
+            return 0
+        output('Checking of the :latest tag', result)
