8c482819b05e30fdf02e11715b9e59fdd42f8ddc
Sergey <xxsmotur@gmail.com>
2019-02-10 17:07:05 +0500

    updating of task method

--- src/jams/task.py
+++ src/jams/task.py
@@ -7,11 +7,14 @@
 """
 
 class TaskBase:
-    def __init__(self, name):
+    def __init__(self, name, *args, **kwargs):
         self._name = name
     
     def __str__(self):
         return self._name
+    
+    def __getattr__(self, name):
+        return '{0} call'.format(name)
     
     def weight(self):
         """return weight of the task in the case
@@ -20,5 +23,5 @@ class TaskBase:
         return 0
 
 class Task(TaskBase):
-    def __init__(self, name):
+    def __init__(self, name, *args,**kwargs):
         super().__init__(name)
