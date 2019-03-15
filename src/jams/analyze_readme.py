c63abf6bf2733807baf493e8f408258f5ac2b6b0
Sergey <xxsmotur@gmail.com>
2019-03-15 22:44:54 +0500

    fix typos

--- src/jams/analyze_readme.py
+++ src/jams/analyze_readme.py
@@ -20,7 +20,7 @@ class AnalyzeReadme(Checker):
         """check_ci provides checking of README.md
         containce badge with ci provider
         """
-        t := Task('check_ci', action=action)
+        t = Task('check_ci', action=repo)
         checkers = [self._check_ci(repo),
                     self._check_quality_report(),
                     self._check_title(),
