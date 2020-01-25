4a23daad8d49e702e43eec99a5de1eb4299401fd
Sergey <xxsmotur@gmail.com>
2020-01-25 23:56:41 +0500

    add check for the repo

---
+++ src/jams/analyzers/repo.py
@@ -0,0 +1,30 @@
+from checker import Checker
+from output import output
+from task import Task
+import requests
+from urlextract import URLExtract
+from spellchecker import SpellChecker
+
+class AnalyzeRepo(Checker):
+    def __init__(self, content, url=None, provider=None):
+        self._content = str(content)
+        super().__init__(str(content), url=url)
+    
+    def name(self):
+        return 'repo'
+
+    def start_message(self):
+        """return message before start of repo checks
+        """
+        print('>Checking of the Repo\n')
+
+    def check(self, repo, **kwargs):
+        """check provides checking of Repo
+        """
+        config = kwargs.get('config')
+        checkers = self._make_checkers(names=self._get_checkers_names_from_cfg(config))
+        self.score.add_total_checks(len(checkers))
+        return sum(checkers)
+    
+    def _default_checkers(self):
+        return []
